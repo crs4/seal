d8818d2c3f93ff38550f37ba7ce0a3abe128f211
Luca Pireddu <pireddu@crs4.it>
2011-04-08 15:56:40 +0200

    * added GPL licence notice and copyright * added README and AUTHORS files

---
+++ README.txt
@@ -0,0 +1,27 @@
+
+==================
+ReadSort
+==================
+
+
+ReadSort is a Hadoop utility to sort read alignments.
+
+Please see the full documentation at  docs/_build/html/index.html.
+
+Authors
+++++++++++++
+
+ReadSort was been written by:
+  * Luca Pireddu <luca.pireddu@crs4.it>
+
+
+License
+++++++++
+
+ReadSort and its components are released under the `GPLv3 license <http://www.gnu.org/licenses/gpl.html>`_.
+
+
+Copyright
+++++++++++
+
+Copyright CRS4, 2011.
--- src/it/crs4/mr/read_sort/ReadSort.java
+++ src/it/crs4/mr/read_sort/ReadSort.java
@@ -1,20 +1,19 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright (C) 2011 CRS4.
+// 
+// This file is part of ReadSort.
+// 
+// ReadSort is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+// 
+// ReadSort is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+// for more details.
+// 
+// You should have received a copy of the GNU General Public License along
+// with ReadSort.  If not, see <http://www.gnu.org/licenses/>.
 
 package it.crs4.mr.read_sort;
 import it.crs4.mr.read_sort.BwaRefAnnotation;
@@ -49,12 +48,6 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
-/**
- * Chooses points, launches the job, and waits for it to finish. 
- * <p>
- * To run the program: 
- * <b>bin/hadoop jar ReadSort.jar -ann file:///abs/path/to/bwa_reference.ann in-dir out-dir</b>
- */
 public class ReadSort extends Configured implements Tool {
 
 	public static final String REF_ANN_PROP_NAME = "readsort.reference.ann";
