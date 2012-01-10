d5700830fc798bf1fd88b607b8459ad3d1d13684
Luca Pireddu <pireddu@crs4.it>
2012-01-10 23:41:10 +0100

    Expand VariantRegion to include a length and a position.

--- src/it/crs4/seal/recab/VariantRegion.java
+++ src/it/crs4/seal/recab/VariantRegion.java
@@ -23,24 +23,50 @@ package it.crs4.seal.recab;
 public class VariantRegion
 {
 	private String contigName;
-	private int position;
+	private int startPos;
+	private int length;
 
 	public VariantRegion() {}
-	public VariantRegion(String contig, int pos)
+	public VariantRegion(String contig, int startPos)
 	{
+		this(contig, startPos, 1);
+	}
+
+	public VariantRegion(String contig, int startPos, int length)
+	{
+		if (startPos <= 0)
+			throw new IllegalArgumentException("Variant position must be > 0");
+		if (length <= 0)
+			throw new IllegalArgumentException("Variant length  must be > 0");
+
 		contigName = contig;
-		position = pos;
+		this.startPos = startPos;
+		this.length = length;
 	}
 
 	public String getContigName() { return contigName; }
-	public int getPosition() { return position; }
+	public int getPosition() { return startPos; }
 
 	public void setContigName(String name) { contigName = name; }
-	public void setPosition(int pos) { position = pos; }
+
+	public void setPosition(int pos) 
+	{
+		if (pos <= 0)
+			throw new IllegalArgumentException("Variant position must be > 0");
+	 	startPos = pos; 
+	}
+
+	public void setLength(int len) 
+	{
+		if (len <= 0)
+			throw new IllegalArgumentException("Variant length  must be > 0");
+		length = len; 
+	}
 
 	public void set(VariantRegion other)
 	{
 		contigName = other.contigName;
-		position = other.position;
+		startPos = other.startPos;
+		length = other.length;
 	}
 }
