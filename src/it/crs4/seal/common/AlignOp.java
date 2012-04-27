816109b30388261efdcf0fc99a90dbc9802786eb
Luca Pireddu <pireddu@crs4.it>
2012-04-27 18:12:41 +0200

    Add cigar to/from string methods to AlignOp, with unit tests

--- src/it/crs4/seal/common/AlignOp.java
+++ src/it/crs4/seal/common/AlignOp.java
@@ -17,6 +17,10 @@
 
 package it.crs4.seal.common;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.*;
+
 public class AlignOp {
 
 	public static enum Type {
@@ -27,6 +31,28 @@ public class AlignOp {
 		HardClip,
 		Skip,
 		Pad;
+
+		public char getSymbol()
+		{
+			switch (this) {
+			case Match:
+				return 'M';
+			case Insert:
+				return 'I';
+			case Delete:
+				return 'D';
+			case SoftClip:
+				return 'S';
+			case HardClip:
+				return 'H';
+			case Skip:
+				return 'N';
+			case Pad:
+				return 'P';
+			default:
+				throw new RuntimeException("BUG!  Missing cigar operator in AlignOp::Type::getSymbol.");
+			}
+		}
 
 		public static Type fromSymbol(String sym) {
 			if (sym.length() != 1)
@@ -57,6 +83,8 @@ public class AlignOp {
 		}
 	}
 
+	protected static final Pattern CigarElementPattern = Pattern.compile("(\\d+)([MIDNSHP])");
+
 	private Type op;
 	private int len;
 
@@ -80,4 +108,46 @@ public class AlignOp {
 	}
 
 	public String toString() { return "(" + op + "," + len + ")"; }
+
+	/**
+	 * Compute the SAM-style CIGAR string for the given alignment.
+	 */
+	public static String cigarStr(List<AlignOp> alignment)
+	{
+		if (alignment == null || alignment.isEmpty())
+			return "*";
+
+		StringBuilder builder = new StringBuilder(50);
+
+		for (AlignOp op: alignment)
+			builder.append(op.getLen()).append(op.getType().getSymbol());
+
+		return builder.toString();
+	}
+
+	public static List<AlignOp> scanCigar(String cigar)
+	{
+		if ("*".equals(cigar))
+			return new ArrayList<AlignOp>(0);
+		else
+		{
+			ArrayList<AlignOp> result = new ArrayList<AlignOp>(5);
+			Matcher m = CigarElementPattern.matcher(cigar);
+
+			int lastPositionMatched = 0;
+			while (m.find())
+			{
+				result.add( new AlignOp(AlignOp.Type.fromSymbol(m.group(2)), Integer.parseInt(m.group(1))) );
+				lastPositionMatched = m.end();
+			}
+
+			if (lastPositionMatched < cigar.length())
+				throw new FormatException("Invalid CIGAR pattern " + cigar);
+
+			if (result.isEmpty())
+				throw new FormatException("Unable to parse any alignments from CIGAR pattern " + cigar);
+
+			return result;
+		}
+	}
 }
---
+++ tests/it/crs4/seal/common/TestAlignOp.java
@@ -0,0 +1,94 @@
+// Copyright (C) 2011-2012 CRS4.
+//
+// This file is part of Seal.
+//
+// Seal is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// Seal is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with Seal.  If not, see <http://www.gnu.org/licenses/>.
+
+package tests.it.crs4.seal.common;
+
+import it.crs4.seal.common.AlignOp;
+import it.crs4.seal.common.FormatException;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class TestAlignOp
+{
+	@Test(expected=FormatException.class)
+	public void testInvalidCigar1()
+	{
+		AlignOp.scanCigar("pippo");
+	}
+
+	@Test(expected=FormatException.class)
+	public void testInvalidCigar2()
+	{
+		AlignOp.scanCigar("10");
+	}
+
+	@Test(expected=FormatException.class)
+	public void testInvalidCigar3()
+	{
+		AlignOp.scanCigar("M");
+	}
+
+	@Test
+	public void testSimpleCigar()
+	{
+		List<AlignOp> alignment = AlignOp.scanCigar("91M");
+		assertEquals(1, alignment.size());
+		assertEquals(new AlignOp(AlignOp.Type.Match, 91), alignment.get(0));
+	}
+
+	@Test
+	public void testMIMCigar()
+	{
+		List<AlignOp> alignment = AlignOp.scanCigar("5M4I8M");
+		assertEquals(3, alignment.size());
+		assertEquals(new AlignOp(AlignOp.Type.Match, 5), alignment.get(0));
+		assertEquals(new AlignOp(AlignOp.Type.Insert, 4), alignment.get(1));
+		assertEquals(new AlignOp(AlignOp.Type.Match, 8), alignment.get(2));
+	}
+
+	@Test
+	public void testMDMCigar()
+	{
+		List<AlignOp> alignment = AlignOp.scanCigar("5M2D3M");
+		assertEquals(3, alignment.size());
+		assertEquals(new AlignOp(AlignOp.Type.Match, 5), alignment.get(0));
+		assertEquals(new AlignOp(AlignOp.Type.Delete, 2), alignment.get(1));
+		assertEquals(new AlignOp(AlignOp.Type.Match, 3), alignment.get(2));
+	}
+
+	@Test
+	public void testSMCigar()
+	{
+		List<AlignOp> alignment = AlignOp.scanCigar("4S8M");
+		assertEquals(2, alignment.size());
+		assertEquals(new AlignOp(AlignOp.Type.SoftClip, 4), alignment.get(0));
+		assertEquals(new AlignOp(AlignOp.Type.Match, 8), alignment.get(1));
+	}
+
+	@Test
+	public void testCigarStr()
+	{
+		List<AlignOp> alignment = new ArrayList<AlignOp>(2);
+		alignment.add(new AlignOp(AlignOp.Type.SoftClip, 4));
+		alignment.add(new AlignOp(AlignOp.Type.Match, 8));
+		assertEquals("4S8M", AlignOp.cigarStr(alignment));
+	}
+}
