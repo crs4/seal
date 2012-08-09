241759f3b53c3dc6cde264c415a66c8dd27d2ca1
Luca Pireddu <pireddu@crs4.it>
2012-08-09 18:36:18 +0200

    Remove unnecessary import

--- src/it/crs4/seal/demux/DemuxOutputFormat.java
+++ src/it/crs4/seal/demux/DemuxOutputFormat.java
@@ -25,7 +25,6 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.lib.MultipleTextOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.RecordWriter;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
