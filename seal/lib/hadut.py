b38f43609b49e4c000e53bd8783eaeeedc548039
Luca Pireddu <pireddu@crs4.it>
2016-02-15 12:47:31 +0100

    Remove commented or dead code

--- seal/lib/hadut.py
+++ seal/lib/hadut.py
@@ -20,28 +20,10 @@ import pydoop
 
 import copy
 import os
-import sys
 import subprocess
-
-#def run_hadoop_cmd_e(cmd, properties=None, args_list=[]):
-#	retcode = run_hadoop_cmd(cmd, properties, args_list)
-#	if retcode != 0:
-#		raise RuntimeError("Error running Hadoop command")
-
-#def run_hadoop_cmd(cmd, properties=None, args_list=[]):
-#	args = [pydoop.hadoop_exec(), cmd]
-#	if properties:
-#		args += __construct_property_args(properties)
-#	args += map(str, args_list) # only string arguments are allowed
-#	return subprocess.call(args)
 
 def __construct_property_args(prop_dict):
     return sum(map(lambda pair: ["-D", "%s=%s" % pair], prop_dict.iteritems()), []) # sum flattens the list
-
-#def run_class_e(class_name, additional_cp=None, properties=None, args_list=[]):
-#	retcode = run_class(class_name, additional_cp, properties, args_list)
-#	if retcode != 0:
-#		raise RuntimeError("Error running Hadoop class")
 
 def run_pipes(executable, input_path, output_path, properties=None, args_list=[]):
     args = [pydoop.hadoop_exec(), "pipes"]
@@ -52,37 +34,6 @@ def run_pipes(executable, input_path, output_path, properties=None, args_list=[]
     args.extend(["-input", input_path, "-output", output_path])
     args.extend(args_list)
     return subprocess.call(args)
-
-
-#def run_class(class_name, additional_cp=None, properties=None, args_list=[]):
-#	"""
-#	Run a class that needs the Hadoop jars in its class path
-#	"""
-#	args = [pydoop.hadoop_exec(), class_name]
-#	if additional_cp:
-#		env = copy.copy(os.environ)
-#		if type(additional_cp) == str: # wrap a single class path in a list
-#			additional_cp = [additional_cp]
-#		# Pass this classpath string to hadoop through the HADOOP_CLASSPATH
-#		# environment variable.  If HADOOP_CLASSPATH is already defined, we'll
-#		# append our values to it.
-#		if env.has_key('HADOOP_CLASSPATH'):
-#			additional_cp.insert(0, env['HADOOP_CLASSPATH'])
-#		env['HADOOP_CLASSPATH'] = ":".join(additional_cp)
-#	else:
-#		env = os.environ
-#	if properties:
-#		args.extend( __construct_property_args(properties) )
-#	args.extend(args_list)
-#	return subprocess.call(args, env=env)
-#
-#def find_jar(jar_name, root_path=None):
-#	root = root_path or os.getcwd()
-#	paths = (root, os.path.join(root, "build"), "/usr/share/java")
-#	for path in [ os.path.join(path, jar_name) for path in paths ]:
-#		if os.path.exists(path):
-#			return path
-#	return None
 
 def run_hadoop_jar(jar, class_name=None, additional_cp=None, properties=None, args_list=[]):
     """
