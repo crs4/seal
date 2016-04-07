2c323f043935dc92c4eb563fa6d351ada42b5f64
Francesco Versaci <cesco@crs4.it>
2016-04-07 11:37:01 +0200

    got rid of old in/output formats

--- main/scala/HReaders.scala
+++ main/scala/HReaders.scala
@@ -1,46 +1,15 @@
-package bclconverter.blockhadoop
+package bclconverter.hreader
 
 import org.apache.flink.streaming.api.scala._
 import bclconverter.{FlinkStreamProvider => FP}
-import java.io.{File, BufferedInputStream, FileInputStream}
-import org.apache.flink.core.fs.FileSystem.WriteMode
-import org.apache.flink.api.common.io.{SerializedInputFormat, BinaryInputFormat, FileInputFormat, GenericInputFormat}
-import org.apache.flink.types.{ByteValue, StringValue}
-import org.apache.flink.configuration.Configuration
-import org.apache.flink.core.memory.DataInputView
-import org.apache.flink.core.fs.FileInputSplit
-import org.apache.flink.core.fs.Path
-import org.apache.flink.api.common.io.{SerializedOutputFormat, BinaryOutputFormat, FileOutputFormat}
-import org.apache.flink.core.memory.DataOutputView
-
-import java.io.RandomAccessFile
-import java.nio.MappedByteBuffer
-import java.nio.ByteBuffer
-import java.nio.channels.FileChannel
-import org.apache.flink.core.io.GenericInputSplit
-
 import org.apache.hadoop.mapreduce.lib.input.{FileInputFormat => HFileInputFormat, FileSplit}
 import org.apache.hadoop.mapreduce.lib.output.{FileOutputFormat => HFileOutputFormat}
 import org.apache.hadoop.mapreduce.{InputSplit, TaskAttemptContext, RecordReader}
 import org.apache.flink.api.scala.hadoop.mapreduce.{HadoopInputFormat, HadoopOutputFormat}
 import org.apache.hadoop.mapreduce.Job
-import org.apache.hadoop.conf.{Configuration => HConfiguration}
 import org.apache.hadoop.fs.{Path => HPath}
 import org.apache.hadoop.fs.{FSDataInputStream, FSDataOutputStream}
 import org.apache.hadoop.mapreduce.RecordWriter
-import org.apache.flink.streaming.api.functions.source.FileMonitoringFunction.WatchType
-import org.apache.flink.streaming.api.windowing.assigners.GlobalWindows
-import org.apache.flink.streaming.api.functions.source.SourceFunction.SourceContext
-import org.apache.flink.streaming.api.functions.source.SourceFunction
-import org.apache.flink.streaming.api.functions.co.CoFlatMapFunction
-import org.apache.flink.util.Collector
-import org.apache.flink.streaming.api.scala.function.AllWindowFunction
-import org.apache.flink.streaming.api.windowing.windows.{Window, GlobalWindow, TimeWindow}
-import org.apache.flink.streaming.api.windowing.assigners.{GlobalWindows, TumblingTimeWindows, SlidingTimeWindows, SlidingEventTimeWindows}
-import scala.util.Random
-import org.apache.flink.streaming.api.windowing.time.Time
-import org.apache.flink.streaming.api.operators.OneInputStreamOperator
-import org.apache.flink.streaming.api.windowing.triggers.{CountTrigger, PurgingTrigger, EventTimeTrigger}
 import org.apache.flink.api.common.functions.MapFunction
 
 
@@ -50,63 +19,7 @@ object BCL {
   }
 }
 
-
 case class BCL(b : Byte) {
-}
-
-class Hout2 extends HFileOutputFormat[Void, Byte] {
-  val bsize = 64*1024
-  var buf = new Array[Byte](bsize)
-  var lpos = 0 // local pos
-  var fileOut : FSDataOutputStream = null
-  class RW extends RecordWriter[Void, Byte] {
-    println("-------> new record writer created")
-
-    def close(ta : TaskAttemptContext) = {
-      fileOut.write(buf, 0, lpos)
-      fileOut.close
-    }
-    def write(void: Void, what: Byte) = {
-      buf(lpos) = what
-      lpos += 1
-      if (lpos == bsize) {
-        lpos = 0        
-        for (b <- buf.grouped(256)){
-          fileOut.write(b)
-        }
-      }
-    }
-  }
-  def getRecordWriter(job: TaskAttemptContext) : RecordWriter[Void, Byte] = {
-    val conf = job.getConfiguration
-    val file = getDefaultWorkFile(job, "")
-    val fs = file.getFileSystem(conf)
-    fileOut = fs.create(file, false)
-
-    new RW
-  }
-}
-
-
-
-class Hout extends HFileOutputFormat[Void, Byte] {
-  var fileOut : FSDataOutputStream = null
-  class RW extends RecordWriter[Void, Byte] {
-    def close(ta : TaskAttemptContext) = {
-      fileOut.close
-    }
-    def write(void: Void, what: Byte) = {
-      fileOut.writeByte(what)
-    }
-  }
-  def getRecordWriter(job: TaskAttemptContext) : RecordWriter[Void, Byte] = {
-    val conf = job.getConfiguration
-    val file = getDefaultWorkFile(job, "")
-    val fs = file.getFileSystem(conf)
-    fileOut = fs.create(file, false)
-
-    new RW
-  }
 }
 
 
@@ -189,117 +102,9 @@ class BHin extends HFileInputFormat[Void, Array[Byte]] {
 }
 
 
-class Counter extends SourceFunction[Byte] {
-  var go = true
-  var count = 0
-  var num : Byte = 0
-  val rep = 128*1024*1024
-
-  def cancel = {
-    go = false
-  }
-
-  def run(ctx : SourceContext[Byte]){
-    while (go && count < rep) {
-      ctx.collect(num)
-      count += 1
-      num = (num + 1).toByte
-    }
-  }
-}
-
-class BCounter extends SourceFunction[Array[Byte]] {
-  var go = true
-  var count = 0
-  val bsize = 1024
-  val rep = 128*1024*1024/bsize
-  
-  val buf = new Array[Byte](bsize)
-  (new Random).nextBytes(buf)
-
-  def cancel = {
-    go = false
-  }
-
-  def run(ctx : SourceContext[Array[Byte]]){
-    while (go && count < rep) {
-      ctx.collect(buf)
-      count += 1
-    }
-  }
-}
-
-class BBCounter extends SourceFunction[Array[BCL]] {
-  var go = true
-  var count = 0
-  val bsize = 1024
-  val rep = 128*1024*1024/bsize
-  
-  val buf = new Array[Byte](bsize)
-  (new Random).nextBytes(buf)
-
-  val bbuf = buf.map(x => BCL(x))
-
-  def cancel = {
-    go = false
-  }
-
-  def run(ctx : SourceContext[Array[BCL]]){
-    while (go && count < rep) {
-      ctx.collect(bbuf)
-      count += 1
-    }
-  }
-}
-
-object Aggr {
-  val coso = new Array[Byte](256*32)
-}
-
-class Aggr[W <: Window] extends AllWindowFunction[Byte, Array[Byte], W] {
-  def apply(win : W, in : Iterable[Byte], out : Collector[Array[Byte]]) = {
-    out.collect(in.toArray) ///// ---> in.toArray
-  }
-}
-
-class Aggr2[W <: Window] extends AllWindowFunction[Array[Byte], Array[Byte], W] {
-  def apply(win : W, in : Iterable[Array[Byte]], out : Collector[Array[Byte]]) = {
-    for (b <- in){
-      out.collect(b)
-    }
-  }
-}
-
-class BAggr2[W <: Window] extends AllWindowFunction[Array[BCL], Array[Byte], W] {
-  def apply(win : W, in : Iterable[Array[BCL]], out : Collector[Array[Byte]]) = {
-    for (x <- in){
-      val a = x.map(_.b).toArray
-      out.collect(a)
-    }
-  }
-}
-
-
-class BAggr[W <: Window] extends AllWindowFunction[BCL, Array[Byte], W] {
-  def apply(win : W, in : Iterable[BCL], out : Collector[Array[Byte]]) = {
-    val a = in.map(_.b).toArray
-    out.collect(a)
-  }
-}
-
-object Filtro {
-  val random = new Random
-}
-
 class Filtro extends MapFunction[Array[Byte], Array[Byte]]{
   def map(in : Array[Byte]) : Array[Byte] = {
     in.map(b => (b & 0x12).toByte)
-  }
-}
-
-class Filtro2 extends MapFunction[Byte, Byte]{
-  def map(b : Byte) : Byte = {
-    (b & 0x12).toByte
   }
 }
 
@@ -322,9 +127,6 @@ object HReader {
     // HFileInputFormat.setMaxInputSplitSize(job, 1024*1024)
     val d = FP.env.createInput(hin) // DataStream[(Void, Array[Byte])]
 
-    // val cow = FP.env.addSource(new Counter)
-
-
     val hout = new HadoopOutputFormat(new BHout, job)
     val hopath = new HPath(fout)
     HFileOutputFormat.setOutputPath(job, hopath)
@@ -335,15 +137,8 @@ object HReader {
       .map(new Filtro)
     val sout = sin.map(x => (void, x))
 
-    /*
-    val sout = cow
-      // .map(b => BCL(b))
-      .countWindowAll(bsize).apply(new Aggr[GlobalWindow])
-      .map(x => (void, x))
-    */
 
     sout.writeUsingOutputFormat(hout)
-
     FP.env.execute
 
     hout.finalizeGlobal(1)
